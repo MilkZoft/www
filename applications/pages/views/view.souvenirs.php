db9bef1bf003eb06fa76459b35154c95fb5a1a2d
Héctor Cerón Figueroa <ceron@milkzoft.com>
2011-11-15 02:07:17 -0600

    Más Vistas

--- applications/pages/views/view.souvenirs.php
+++ applications/pages/views/view.souvenirs.php
@@ -1,3 +1,5 @@
+			<?php $imagesPath = _webURL._sh."www"._sh._applications._sh."pages"._sh."views"._sh."images"._sh; ?>
+			
 			<a name="top"></a>
 
 			<div id="pages" class="grid_9">		
@@ -6,17 +8,56 @@
 				<h3>Artesanías y pinturas</h3>
 							
 				<div class="item grid_3 push_10p">
-					<img src="<?php print _webURL/ ?>">
+					<div>
+						<img src="<?php print $imagesPath."catrina.png"; ?>" />
+						<span class="bold">
+							<span class="uppercased">Catrina</span> Barro $450.00
+						</span>
+					</div>
+					
+					<div>
+						<img src="<?php print $imagesPath."sol.png"; ?>" />
+						<span class="bold">
+							<span class="uppercased">Sol</span> Barro $60.00
+						</span>
+					</div>
+					
+					<div>
+						<img src="<?php print $imagesPath."nopal.png"; ?>">
+					</div>
+					
 				</div>
 				
 				<div class="item grid_3">
-					assaas
+					<div>
+						<img src="<?php print $imagesPath."jarro.png"; ?>">
+					</div>
+					
+					<div>
+						<img src="<?php print $imagesPath."ave.png"; ?>">
+					</div>
+					
+					<div>
+						<img src="<?php print $imagesPath."montana.png"; ?>">
+					</div>
+					
 				</div>
 				
 				<div class="item grid_3">
-					assaas
+					<div>
+						<img src="<?php print $imagesPath."solyluna.png"; ?>">
+					</div>
+					
+					<div>
+						<img src="<?php print $imagesPath."cocina.png"; ?>">
+					</div>
+					
+					<div>
+						<img src="<?php print $imagesPath."callejon.png"; ?>">
+					</div>
+					
 				</div>
-		
+				
 			</div>
 			
 			
