ad3777690b0dabb527274a2d24419861549f2c0d
Carlos Hugo Gonzalez Castell <caarloshugo@gmail.com>
2011-11-24 02:43:41 -0600

    spanish

--- lib/themes/volcanes/right.php
+++ lib/themes/volcanes/right.php
@@ -3,12 +3,12 @@
 					<div id="sidebar-header"></div>
 					<div id="sidebar-menu">
 						<ul>
-							<li class="bordered"><a>* <?php print __("Cabin Rental");?>Renta de Cabañas</a></li>
-							<li class="bordered"><a>* <?php print __("Tours");?>Tours</a></li>
-							<li class="bordered"><a>* <?php print __("Construction of Eco-cabins");?>Construcción de Cabañas Ecológicas</a></li>
-							<li class="bordered"><a>* <?php print __("Construction of tree little houses");?>Construcción de Casitas de juego</a></li>
-							<li class="bordered"><a>* <?php print __("Construction of Green roofs");?>Cubiertas verdes</a></li>
-							<li><a>* <?php print __("Organic products and handcraft");?>Productos orgánicos y artesanales</a></li>
+							<li class="bordered"><a>* <?php print __("Cabin Rental");?></a></li>
+							<li class="bordered"><a>* <?php print __("Tours");?></a></li>
+							<li class="bordered"><a>* <?php print __("Construction of Eco-cabins");?></a></li>
+							<li class="bordered"><a>* <?php print __("Construction of tree little houses");?></a></li>
+							<li class="bordered"><a>* <?php print __("Construction of Green roofs");?></a></li>
+							<li><a>* <?php print __("Organic products and handcraft");?></a></li>
 						</ul>
 					</div>
 					<!-- Sub Sidebar -->
