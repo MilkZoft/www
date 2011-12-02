03e0b30795e39cbcfcb8d13ece83010d9214bee3
Carlos Hugo Gonzalez Castell <caarloshugo@gmail.com>
2011-12-02 00:57:35 -0600

    header

--- lib/themes/ciberpri/header.php
+++ lib/themes/ciberpri/header.php
@@ -36,13 +36,13 @@
 							<div class="header-col">
 								<nav>
 									<ul>
-										<li <?php print (segment(2) === slug(__("Home")) or segment(2) === FALSE) ? 'class="active"' : '' ;?>>
+										<li <?php print (segment(2) === slug(__("Home")) or segment(1) === slug(__("Home")) or (!segment(2) and !segment(1))) ? 'class="active"' : '' ;?>>
 											<a href="<?php print _webBase . _sh . _webLang . _sh . "pages" . _sh . slug(__("Home"));?>"><?php print __("Start");?></a>
 										</li>
-										<li <?php print (segment(2) === slug(__("Who are we?"))) ? 'class="active"' : '' ;?>>
+										<li <?php print (segment(2) === slug(__("Who are we?")) or segment(1) === slug(__("Who are we?"))) ? 'class="active"' : '' ;?>>
 											<a href="<?php print _webBase . _sh . _webLang . _sh . "pages" . _sh . slug(__("Who are we?"));?>"><?php print __("Who are we?");?></a>
 										</li>
-										<li <?php print (segment(2) === slug(__("Directory"))) ? 'class="active"' : '' ;?>>
+										<li <?php print (segment(2) === slug(__("Directory")) or segment(1) === slug(__("Directory"))) ? 'class="active"' : '' ;?>>
 											<a href="<?php print _webBase . _sh . _webLang . _sh . "pages" . _sh . slug(__("Directory"));?>"><?php print __("Directory");?></a>
 										</li>
 										<li <?php print (segment(1) === "afiliate") ? 'class="active"' : '' ;?>>
