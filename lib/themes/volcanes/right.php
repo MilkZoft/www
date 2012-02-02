27f6968c6aeaabbfd3abad1606a97d8afcd6447d
Héctor Cerón Figueroa <ceron@milkzoft.com>
2012-02-01 22:18:45 -0600

    Más Imágenes

--- lib/themes/volcanes/right.php
+++ lib/themes/volcanes/right.php
@@ -7,12 +7,12 @@
 					</div>
 					
 					<div id="sidebar-menu">
-						<ul>
+						<ul>Green Roof Building and Design
 							<li class="bordered"><a href="<?php print _webBase . _sh . _webLang . _sh . "pages"; ?>">* <?php print __("Cabin Rental");?></a></li>
 							<li class="bordered"><a href="<?php print _webBase . _sh . _webLang . _sh . "pages" . _sh . slug(__("Tours"));?>">* <?php print __("Tours");?></a></li>
 							<li class="bordered"><a href="<?php print _webBase . _sh . _webLang . _sh . "pages" . _sh . slug(__("Design and building of ecocabins"));?>">* <?php print __("Design and building of ecocabins");?></a></li>
 							<li class="bordered"><a href="<?php print _webBase . _sh . _webLang . _sh . "pages" . _sh . slug(__("Construction of play houses"));?>">* <?php print __("Construction of play houses");?></a></li>
-							<li class="bordered"><a href="<?php print _webBase . _sh . _webLang . _sh . "pages" . _sh . slug(__("Construction of Green roofs"));?>">* <?php print __("Construction of Green roofs");?></a></li>
+							<li class="bordered"><a href="<?php print _webBase . _sh . _webLang . _sh . "pages" . _sh . slug(__("Green Roof Building and Design"));?>">* <?php print __("Green Roof Building and Design");?></a></li>
 							<li><a href="<?php print _webBase . _sh . _webLang . _sh . "pages" . _sh . slug(__("Alimentation"));?>">* <?php print __("Organic products and handcraft");?></a></li>
 						</ul>
 					</div>
