f2e95ecd7751410100a8ab81942e8d90091b5d0f
Héctor Cerón Figueroa <ceron@milkzoft.com>
2012-02-01 21:37:48 -0600

    Modificación de right.php

--- lib/themes/volcanes/right.php
+++ lib/themes/volcanes/right.php
@@ -8,7 +8,7 @@
 					
 					<div id="sidebar-menu">
 						<ul>
-							<li class="bordered"><a href="<?php print _webBase . _sh . _webLang . _sh . "pages" . _sh . slug(__("Lodgings"));?>">* <?php print __("Cabin Rental");?></a></li>
+							<li class="bordered"><a href="<?php print _webBase . _sh . _webLang . _sh . "pages"; ?>">* <?php print __("Cabin Rental");?></a></li>
 							<li class="bordered"><a href="<?php print _webBase . _sh . _webLang . _sh . "pages" . _sh . slug(__("Tours"));?>">* <?php print __("Tours");?></a></li>
 							<li class="bordered"><a href="<?php print _webBase . _sh . _webLang . _sh . "pages" . _sh . slug(__("Design and building of ecocabins"));?>">* <?php print __("Design and building of ecocabins");?></a></li>
 							<li class="bordered"><a href="<?php print _webBase . _sh . _webLang . _sh . "pages" . _sh . slug(__("Construction of tree little houses"));?>">* <?php print __("Construction of tree little houses");?></a></li>
